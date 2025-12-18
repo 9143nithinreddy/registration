ebe5c394063365291bb07516a59d6a7e62a7d767
Nithin <nithinyadamakanti@gmail.com>
2025-12-18 11:23:48 +0530

    created repository and service for registrations

---
+++ registration/src/main/java/com/registration/service/UserService.java
@@ -0,0 +1,27 @@
+package com.registration.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.registration.model.User;
+import com.registration.repository.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository userrepository;
+	
+	//registration
+	public User registeruser(User user) {
+		return userrepository.save(user);
+	}
+	//getusers
+	public List<User> getAllregistrations(){
+		return userrepository.findAll();
+	}
+	
+
+}
